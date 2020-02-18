d03786dc21b95b78b792bdc869ca617bed7688eb
Shannon Poole <shannon.m.poole@gmail.com>
2020-02-18 14:58:59 -0500

    moved to lib convention instead of src

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import LocalConnection from './src/local-connection.js';
-import SignalClient    from './src/signal-client.js';
-import SignalServer    from './src/signal-server.js';
-import console         from './src/console.js';
+import LocalConnection from './lib/local-connection.js';
+import SignalClient    from './lib/signal-client.js';
+import SignalServer    from './lib/signal-server.js';
+import console         from './lib/console.js';
 
 export { LocalConnection, SignalClient, SignalServer, console };
