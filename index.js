fcce5bb698f0533f96817968035844a0d78fcd88
Shannon Poole <shannon.m.poole@gmail.com>
2020-02-18 14:55:09 -0500

    fix console in index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import LocalConnection from './src/local-connection.js';
 import SignalClient    from './src/signal-client.js';
 import SignalServer    from './src/signal-server.js';
-import console         from './console.js';
+import console         from './src/console.js';
 
 export { LocalConnection, SignalClient, SignalServer, console };
